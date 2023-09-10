d882468a189b00ed862d59c12c891b19b5c69890
Ashrf <ashrftvm1@gmail.com>
2023-09-10 21:16:47 +0400

    calculator final function

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  let res;
+  switch (operator) {
+    case "+":
+      res = num1 + num2;
+      break;
+    case "-":
+      res = num1 - num2;
+      break;
+    case "/":
+      res = num1 / num2;
+      break;
+    case "*":
+      res = num1 * num2;
+      break;
+    default:
+      throw new Error("Operator error");
+  }
+  return res;
+}
 
 module.exports = calculator;
