11b2a2226c5905b75abdec27c49fed9cd033210a
Ashrf <ashrftvm1@gmail.com>
2023-09-15 00:14:38 +0400

    palindrome completed with tests

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,10 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  // Remove all non-alphanumeric characters and convert the string to lowercase
+  const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  // Reverse the string
+  const reversedStr = formattedStr.split("").reverse().join("");
+  // Return whether the formatted string is equal to the reversed string
+  return formattedStr === reversedStr;
+}
 
 module.exports = isPalindrome;
