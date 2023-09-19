179facfde340689d792128a910e24066fbc4828b
45hrf <ashrftvm1@gmail.com>
2023-09-19 22:29:48 +0400

    implemented vowels count function

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,12 @@
-function countVowels() {}
+function countVowels(str) {
+    const lowerStr = [...str.toLowerCase()]
+    let vowelsCount = 0;
+    lowerStr.forEach(element => {
+        if(element === "a" || element === "e" || element === "i" || element === "o" || element === "u"){
+            vowelsCount++
+        }
+    });
+    return vowelsCount
+}
 
 module.exports = countVowels;
