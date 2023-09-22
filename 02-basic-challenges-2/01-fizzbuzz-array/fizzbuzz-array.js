ca25cafe676ceaadd008192adaf2a498ab2b4772
45hrf <ashrftvm1@gmail.com>
2023-09-22 13:57:54 +0400

    implemented fizzbuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    const arr =[]
+    for (let index = 1; index <= num; index++) {
+        if(index % 3 === 0 && index % 5 === 0){
+            arr.push('FizzBuzz')
+        }else if(index % 3 === 0){
+            arr.push('Fizz')
+        }else if (index % 5 === 0){
+            arr.push('Buzz')
+        }else{
+            arr.push(index)
+        }
+    }
+    return arr
+}
 
 module.exports = fizzBuzzArray;
